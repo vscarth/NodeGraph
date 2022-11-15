ff9eff8547f53141f044d94239dd0d091ed177e3
vscarth <victoria.scarth233@gmail.com>
2022-11-15 09:46:00 -0800

    Config files

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-multiple-empty-lines": [2, { max: 10, maxEOF: 1 }],
+  },
+};
